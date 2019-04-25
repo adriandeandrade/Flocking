da110941a640e749f4ce27e3d3d5a06da9d8ed5d
Adrian De Andrade <adriandeandrade98@gmail.com>
2019-04-25 18:14:26 -0400

    Initial commit

---
+++ Assets/Scripts/Behaviors/AlignmentBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Alignment")]
+public class AlignmentBehavior : FlockBehavior
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        // if no neighbors, maintain current alignment
+        if (context.Count == 0)
+        {
+            return agent.transform.up;
+        }
+
+        // Add all points together and average
+        Vector2 alignementMove = Vector2.zero;
+
+        foreach (Transform item in context)
+        {
+            alignementMove += (Vector2)item.transform.up;
+        }
+
+        alignementMove /= context.Count;
+
+        return alignementMove;
+    }
+}
